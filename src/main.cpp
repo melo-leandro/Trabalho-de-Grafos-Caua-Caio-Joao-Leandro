12249326f8c81b724d5ecee439e8edf18f327575
Cauã Moreno <caua.moreno@estudante.ufjf.br>
2025-08-10 17:41:54 -0300

    dados dos experimentos

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,13 @@ int main(int argc, char *argv[]) {
     srand(time(nullptr));
     
     // Gera tabela com valores absolutos
-    Guloso::executar_experimentos_consolidado("src/instancias_t2", "src/output/tabela_qualidade.txt");
+    Guloso::executar_experimentos_qualidades("src/instancias_t2", "src/output/tabela_qualidade.txt");
     
     // Gera tabela com diferenças percentuais
     Guloso::executar_experimentos_percentuais("src/instancias_t2", "src/output/tabela_percentual.txt");
+    
+    // Gera tabela com tempos de execução
+    Guloso::executar_experimentos_tempos("src/instancias_t2", "src/output/tabela_tempos.txt");
     
     return 0;
 }
